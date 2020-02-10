3ccb06bd97bbe8d27a686a5595368179e49e496a
John Mutuma (Thoguo_) <johnmutuma5@gmail.com>
2020-02-10 11:44:43 +0300

    implement merge sort

--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
-console.log("Hello world!");
+import { sort } from './mergeSort';
+
+const arrayToSort = [4, 39, 84, 9, 3, 3, 2, 1, -1, 5];
+const sortedArray: number[] = sort(arrayToSort);
+console.log(`Merge sort ${arrayToSort}: `, sortedArray);
---
+++ mergeSort.ts
@@ -0,0 +1,46 @@
+export function sort(sortArray: number[]): number[] {
+  const lower = 0;
+  const upper = sortArray.length - 1;
+
+  const sortedResult = mergeSort(sortArray, lower, upper);
+  return sortedResult;
+}
+
+
+function mergeSort(array: number[], lower: number, upper: number): number[] {
+  if(upper <= lower){
+    return [array[upper]];
+  }
+  const midpoint = Math.floor((upper + lower) / 2);
+
+  const right = mergeSort(array, lower, midpoint);
+  const left = mergeSort(array, midpoint + 1, upper);
+
+  const mergedResult = merge(right, left);
+  return mergedResult;
+}
+
+function merge(right: number[], left: number[]) {
+  const result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while(rightIndex < right.length && leftIndex < left.length){
+    if(right[rightIndex] <= left[leftIndex]) {
+      result.push(right[rightIndex])
+      rightIndex++;
+    } else {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    }
+  }
+  while(leftIndex < left.length) {
+    result.push(left[leftIndex])
+    leftIndex++;
+  }
+  while(rightIndex < right.length) {
+    result.push(right[rightIndex])
+    rightIndex++;
+  }
+  return result;
+}
