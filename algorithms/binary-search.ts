eadbe65526c26da110ae279d0d5a72ffde8ad04e
John Mutuma (Thoguo_) <johnmutuma5@gmail.com>
2020-02-11 14:54:20 +0300

    add binary search implementation

---
+++ algorithms/binary-search.ts
@@ -0,0 +1,48 @@
+/**
+ *
+ *
+ *   .oPYo.  o                             .oPYo.                            8      
+ *   8   `8                                8                                 8      
+ *  o8YooP' o8 odYo. .oPYo. oPYo. o    o   `Yooo. .oPYo. .oPYo. oPYo. .oPYo. 8oPYo. 
+ *   8   `b  8 8' `8 .oooo8 8  `' 8    8       `8 8oooo8 .oooo8 8  `' 8    ' 8    8 
+ *   8    8  8 8   8 8    8 8     8    8        8 8.     8    8 8     8    . 8    8 
+ *   8oooP'  8 8   8 `YooP8 8     `YooP8   `YooP' `Yooo' `YooP8 8     `YooP' 8    8 
+ *  :......::....::..:.....:..:::::....8 :::.....::.....::.....:..:::::.....:..:::..
+ *  ::::::::::::::::::::::::::::::::ooP'.:::::::::::::::::::::::::::::::::::::::::::
+ *  ::::::::::::::::::::::::::::::::...:::::::::::::::::::::::::::::::::::::::::::::
+ *
+ *  Binary search implementation: Feb 2020
+ *
+ */
+
+import { sort as mergeSort } from './merge-sort';
+
+/**
+ *
+ * A binary search will search for an item in a sorted array. That is important. The array has to be
+ * sorted
+ *
+ *
+ */
+export function search(term: number, container: number[]): boolean {
+  const lower = 0;
+  const upper = container.length - 1;
+  return binarySearch(term, mergeSort(container), lower, upper);
+}
+
+function binarySearch(term: number, container: number[], lower: number, upper:number): boolean {
+  const midpoint = Math.floor((lower + upper) / 2);
+  const midpointValue = container[midpoint];
+
+  if(midpointValue === term)
+    return true;
+
+  if(lower === upper)
+    return false;
+
+  if(midpointValue < term) // explore right
+    return binarySearch(term, container, midpoint + 1, upper);
+  else
+    return binarySearch(term, container, lower, midpoint - 1);
+
+}
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { sort as mergeSort } from './algorithms/merge-sort';
 import { isPalindrome } from './algorithms/palindrome';
+import { search as binarySearch } from './algorithms/binary-search';
 
 const arrayToSort = [4, 39, 84, 9, 3, 3, 2, 1, -1, 5];
 const sortedArray: number[] = mergeSort(arrayToSort);
@@ -9,3 +10,7 @@ const word = 'Deleveled';
 const word2 = 'notPalindrome';
 console.log(`Is the word \`${word}\` a palindrome? ${isPalindrome(word)}`);
 console.log(`Is the word \`${word2}\` a palindrome? ${isPalindrome(word2)}`);
+
+const arrayToSearch = [4, 39, 84, 9, 3, 8, 3, 2, 1, -1, 5];
+const term = 1;
+console.log(`Is ${term} in ${arrayToSearch}? Binary Search: `, binarySearch(term, arrayToSearch));
