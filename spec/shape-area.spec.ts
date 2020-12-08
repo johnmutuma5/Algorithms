52ea51ec319c5284fb9322bead84707988a32592
John Mutuma <johnmutuma5@gmail.com>
2020-12-08 16:27:08 +0300

    added test for shape area interesting polygons

---
+++ spec/shape-area.spec.ts
@@ -0,0 +1,14 @@
+import {shapeArea, shapeAreaNoRecursive} from "../algorithms/shape-area";
+
+describe('Shape Area Interesting polygons', () => {
+  it('Should return 41 when n = 5', () => {
+    expect(shapeArea(5)).toEqual(41);
+    expect(shapeAreaNoRecursive(5)).toEqual(41);
+  });
+
+  it('Should return 19801 when n = 100', () => {
+    expect(shapeArea(100)).toEqual(19801);
+    expect(shapeAreaNoRecursive(100)).toEqual(19801);
+  });
+});
+
